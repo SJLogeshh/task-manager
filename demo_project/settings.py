e147be71f1a60934ba77c154a096c633bb8d9532
Logesh Sengodan <logesh.sengodan@kumaran.com>
2026-04-30 14:31:04 +0530

    Switch SQLite to PostgreSQL

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'demo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'dbadmin',
+        'PASSWORD': 'logesh@2001',
+        'HOST': 'task-db-sj.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
